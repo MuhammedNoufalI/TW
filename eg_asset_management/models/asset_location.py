e639fd541221954dd078c9e228924300ff558697
muhammed noufal <60594455+MuhammedNoufalI@users.noreply.github.com>
2024-08-05 08:22:36 +0000

    updt eg_asset location code

--- eg_asset_management/models/asset_location.py
+++ eg_asset_management/models/asset_location.py
@@ -6,6 +6,7 @@ class AssetLocation(models.Model):
     _description = "Asset Location"
 
     name = fields.Char(string="Name")
+    location_code = fields.Char(string="")
     is_default = fields.Boolean(string="Default")
     is_scrap = fields.Boolean(string="Scrap")
     asset_line = fields.One2many(comodel_name="asset.detail", inverse_name="location_id")
