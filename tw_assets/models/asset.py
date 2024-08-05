8f2a72b76362ffdc2b6d2acbd638ca45938a01f6
muhammed noufal <60594455+MuhammedNoufalI@users.noreply.github.com>
2024-08-05 07:23:06 +0000

    -v1 tw_assets

--- tw_assets/models/asset.py
+++ tw_assets/models/asset.py
@@ -1,7 +1,7 @@
 from odoo import models, api
 
 class AssetAsset(models.Model):
-    _inherit = 'eg.asset'
+    _inherit = 'eg.asset'  # Ensure this is the correct model name for the assets in the `eg_asset_management` module
 
     @api.model
     def create(self, vals):
@@ -10,8 +10,9 @@ class AssetAsset(models.Model):
 
         if location_id and category_id:
             location = self.env['res.partner'].browse(location_id)
-            category = self.env['eg.asset.category'].browse(category_id)
+            category = self.env['asset.category'].browse(category_id)
             sequence_code = f"TW/{location.name}/{category.short_code}/%(range_year)s"
 
             vals['code'] = self.env['ir.sequence'].next_by_code(sequence_code) or '/'
         return super(AssetAsset, self).create(vals)
+
